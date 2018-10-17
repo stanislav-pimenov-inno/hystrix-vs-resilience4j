7b8eae8a3ed83b8f633f2c80604ff733220d5c98
Stanislav Pimenov <v-spimenov@corp.epnet.com>
2018-10-17 13:46:23 +0300

    resilience4j: basic research

--- README.md
+++ README.md
@@ -1,11 +1,42 @@
 # hystrix-vs-resilience4j
 
 
+
 Feature | Hystrix | Resilience4j
 ------- | ------- | ------------
-Circuit Breaker pattern implementation | Y | Y
-Bulkhead pattern implementation | ? | ?
-Integration with Spring Boot 2 | ? | ?
-Prometheus metrics | ? | ?
-Configure bulkhead/circuitbkread within application.yml | ? | ?
-Amount of transitive dependencies | ? | ?
+Bulkhead pattern implementation | ? | Y
+Integration with Spring Boot 2 | ? | Y
+Integration with WebFlux (Reactor) | integrated with JavaRX | Y
+Prometheus metrics | ? | N ( meaningful metrics exposed just for CircuitBreaker) 
+Configure bulkhead within application.yml from the box | ? | N (applicable for Ratelimiter and CircuitBreaker only)
+Amount of transitive dependencies | ? | low
+
+# Resilience4j
+
+## Test Plan
+
+![](img/test-plan.png)
+
+## Bulkhead
+### Example: max 25 concurrent request
+
+![](img/bulkhead-25concurrent-calls.png)
+
+### Exception count metrics
+```
+http_server_requests_seconds_count{exception="BulkheadFullException",method="GET",status="500",uri="/hello/{api}",} 645.0
+http_server_requests_seconds_sum{exception="BulkheadFullException",method="GET",status="500",uri="/hello/{api}",} 0.457094154
+http_server_requests_seconds_max{exception="BulkheadFullException",method="GET",status="500",uri="/hello/{api}",} 0.0
+```
+
+## RateLimiter
+### Example 10 requests per seconds
+
+![](img/ratemimiter-10rps.png)
+
+### Exception count metrics
+```
+http_server_requests_seconds_count{exception="RequestNotPermitted",method="GET",status="500",uri="/hello/{api}",} 418.0
+http_server_requests_seconds_sum{exception="RequestNotPermitted",method="GET",status="500",uri="/hello/{api}",} 838.681654162
+http_server_requests_seconds_max{exception="RequestNotPermitted",method="GET",status="500",uri="/hello/{api}",} 2.065762051
+```
--- resilience4j-sample/src/main/java/com/ebsco/ebsconext/resilience4jsample/Resilience4jService.java
+++ resilience4j-sample/src/main/java/com/ebsco/ebsconext/resilience4jsample/Resilience4jService.java
@@ -1,11 +1,12 @@
 package com.ebsco.ebsconext.resilience4jsample;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.timelimiter.TimeLimiter;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import java.time.Duration;
 import java.util.function.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ import org.springframework.web.client.RestTemplate;
 public class Resilience4jService {
 
   private CircuitBreaker circuitBreaker;
+  private Bulkhead bulkhead;
+  private RateLimiter rateLimiter;
+
   //private TimeLimiter timeLimiter;
   //private ExecutorService executorService = Executors.newFixedThreadPool(10);
   private Function<String, String> chainedCallable;
@@ -34,13 +38,27 @@ public class Resilience4jService {
 //        .build();
 //    // Create a CircuitBreakerRegistry with a custom global configuration
     //CircuitBreakerRegistry circuitBreakerRegistry = CircuitBreakerRegistry.(circuitBreakerConfig);
+//
+    bulkhead = Bulkhead.of("apiCall", BulkheadConfig.ofDefaults());
+    chainedCallable = Bulkhead.decorateFunction(bulkhead, this::restrictedCall);
 
-    circuitBreaker = CircuitBreaker.of("apiCall", CircuitBreakerConfig.ofDefaults());
-    chainedCallable = CircuitBreaker.decorateFunction(circuitBreaker, this::restrictedCall);
+    circuitBreaker = CircuitBreaker.of("apiCall", CircuitBreakerConfig.custom()
+        .failureRateThreshold(50)
+        .waitDurationInOpenState(Duration.ofMillis(1000))
+        .ringBufferSizeInHalfOpenState(2)
+        .ringBufferSizeInClosedState(2)
+        .build());
+    //chainedCallable = CircuitBreaker.decorateFunction(circuitBreaker, this::restrictedCall);
+
+    rateLimiter = RateLimiter.of("apiCall", RateLimiterConfig.custom()
+        .limitRefreshPeriod(Duration.ofSeconds(1))
+        .limitForPeriod(10)
+        .timeoutDuration(Duration.ofSeconds(2))
+        .build());
+    //chainedCallable = RateLimiter.decorateFunction(rateLimiter, this::restrictedCall);
   }
 
   public String callApi(String api) {
-
     return chainedCallable.apply(api);
   }
 
