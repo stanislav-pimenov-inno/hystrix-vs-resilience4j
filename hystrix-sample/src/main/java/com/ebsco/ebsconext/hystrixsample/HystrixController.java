32f223ffe8f8a3df8e21e3428db36e0605c703d9
Stanislav Pimenov <v-spimenov@corp.epnet.com>
2018-10-17 17:27:32 +0300

    hystrix: added sample project

--- hystrix-sample/src/main/java/com/ebsco/ebsconext/hystrixsample/HystrixController.java
+++ hystrix-sample/src/main/java/com/ebsco/ebsconext/hystrixsample/HystrixController.java
@@ -1,8 +1,20 @@
 package com.ebsco.ebsconext.hystrixsample;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HystrixController {
+
+
+  @Autowired
+  private HystrixService service;
+
+  @GetMapping("/hello/{api}")
+  public String getHello(@PathVariable String api) {
+    return service.callApi(api);
+  }
 
 }
